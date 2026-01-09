68d4f07e3df7872b42960e13e5157ad9b1a6cefb
lgrindei <129519412+lgrindei@users.noreply.github.com>
2026-01-09 09:45:46 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,9 +6,17 @@ int main() {
     i1 = 5;
     p1 = &i1;
 
-    // STUDENTUL TREBUIE SĂ ÎNȚELEAGĂ ACEASTĂ LINIE
-    i2 = 2 * *p1 / 2 + 10;
+    // STUDENTUL COMPLETEAZĂ LINIA DE MAI JOS
+    // Pune valoarea pe care crezi că o va avea i2
+    i2 = ___;   // <- înlocuiește ___ cu numărul tău
 
-    printf("i1 = %d, i2 = %d\n", i1, i2);
-    return 0;
+    // Verificarea corectitudinii
+    if (i1 != 5)
+        return 1;
+
+    if (i2 != 15)
+        return 1;
+
+    return 0;   // PASS
 }
+
